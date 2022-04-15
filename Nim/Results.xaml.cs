dc350487c4af7d45521531e83b3b9efb5f88b0fa
TWatson-Gamester <twatson@student.neumont.edu>
2022-04-14 18:32:26 -0600

    Quick changes to results again

--- Nim/Results.xaml.cs
+++ Nim/Results.xaml.cs
@@ -21,12 +21,18 @@ namespace Nim
     {
         int player1Score = 0;
         int player2Score = 0;
+        Difficulty difficulty;
+        Name names;
 
-        public Results(Name names, string winner)
+        public Results(Name names, string winner, Difficulty difficulty)
         {
             InitializeComponent();
             Player1Label.Content = names.playerOne;
             Player2Label.Content = names.playerTwo;
+            this.difficulty = difficulty;
+            this.names = names;
+            if ((string)Player1Label.Content == winner) player1Score++;
+            else player2Score++;
             updateScores();
         }
 
@@ -39,6 +45,9 @@ namespace Nim
         private void RematchButton_Click(object sender, RoutedEventArgs e)
         {
             //Start up a brand new game
+            Game newWindow = new Game(difficulty, names);
+            newWindow.Show();
+            this.Hide();
         }
 
         private void StartOverButton_Click(object sender, RoutedEventArgs e)
