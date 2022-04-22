9d8d3c0f747e77403861096811fcb2f34c0082a0
Ghostninja2451 <80051664+Ghostninja2451@users.noreply.github.com>
2022-04-21 19:08:48 -0600

    Check Steals method

--- Pente/Game.xaml.cs
+++ Pente/Game.xaml.cs
@@ -21,6 +21,7 @@ namespace Pente
     {
         public string name;
         public string color;
+        public int points;
     } 
 
     public partial class Game : Window
@@ -28,6 +29,8 @@ namespace Pente
         Player p1 = new Player();
         Player p2 = new Player();
         Player currentPlayer;
+
+        public int[,] board = new int[21 , 21];
 
         public Game(string p1Name, string p2Name, string p1Color, string p2Color)
         {
@@ -42,31 +45,13 @@ namespace Pente
 
         private void btn_Click(object sender, RoutedEventArgs e)
         {
-
+            Button btn = sender as Button;
+            //btn.Background = currentPlayer.color;
+            currentPlayer.points += CheckSteals(board, 1, 1); //Needs actual values
+            
+            
         }
-        
-
-        //public void CheckSteals(2D array board, int row, int col)
-        //if steal
-        //change the buttons
-        //increment the steals of current Player
-
-        //public int CheckVictory(2D array board, int row, int col)
-        //checks each direction for the longest line
-        //return the longest line
-
-        
-        public void PlayerWins(string playerName, string color)
-        {
-            Results results = new Results();// Need to add a player who won
-            results.Show();
-            this.Close();
-        }
-        //Opens up results window
-
         //button being pressed
-        //check if valid button pressed
-        //if valid button
         //set colors / button
         //check steals
         //if players steals = 10 then they win
@@ -77,6 +62,146 @@ namespace Pente
         //Case 3: State that they have 3 in a row
         //disable button
         //change player
+
+
+        public int CheckSteals(int[,] board, int row, int col)
+        {
+            //if steal
+            //change the buttons
+            //increment the steals of current Player
+            int playerColor = board[row, col];
+            int check1;
+            int check2;
+            int check3;
+            int pointsGained = 0;
+
+            //Check Up
+            if(row - 3 > 0)
+            {
+                check1 = board[row - 1, col];
+                check2 = board[row - 2, col];
+                check3 = board[row - 3, col];
+                if(playerColor != check1 && playerColor != check2 && playerColor == check3)
+                {
+                    board[row - 1, col] = 0;
+                    board[row - 2, col] = 0;
+                }
+                pointsGained += 2;
+            }
+            //Check Down
+            if (row + 3 < 21) 
+            {
+                check1 = board[row + 1, col];
+                check2 = board[row + 2, col];
+                check3 = board[row + 3, col];
+                if (playerColor != check1 && playerColor != check2 && playerColor == check3)
+                {
+                    board[row + 1, col] = 0;
+                    board[row + 2, col] = 0;
+                }
+                pointsGained += 2;
+            }
+            //Check Left
+            if (col - 3 > 0) 
+            {
+                check1 = board[row, col - 1];
+                check2 = board[row, col - 2];
+                check3 = board[row, col - 3];
+                if (playerColor != check1 && playerColor != check2 && playerColor == check3)
+                {
+                    board[row, col - 1] = 0;
+                    board[row, col - 2] = 0;
+                }
+                pointsGained += 2;
+            }
+            //Check Right
+            if(col + 3 < 21) 
+            {
+                check1 = board[row, col + 1];
+                check2 = board[row, col + 2];
+                check3 = board[row, col + 3];
+                if (playerColor != check1 && playerColor != check2 && playerColor == check3)
+                {
+                    board[row, col + 1] = 0;
+                    board[row, col + 2] = 0;
+                }
+                pointsGained += 2;
+            }
+
+            //Check Top Left
+            if(row - 3 > 0 && col - 3 > 0) 
+            {
+                check1 = board[row - 1, col - 1];
+                check2 = board[row - 2, col - 2];
+                check3 = board[row - 3, col - 3];
+                if (playerColor != check1 && playerColor != check2 && playerColor == check3)
+                {
+                    board[row - 1, col - 1] = 0;
+                    board[row - 2, col - 2] = 0;
+                }
+                pointsGained += 2;
+            }
+            //Check Top Right
+            if(row - 3 > 0 && col + 3 < 21) {
+                check1 = board[row - 1, col + 1];
+                check2 = board[row - 2, col + 2];
+                check3 = board[row - 3, col + 3];
+                if (playerColor != check1 && playerColor != check2 && playerColor == check3)
+                {
+                    board[row - 1, col + 1] = 0;
+                    board[row - 2, col + 2] = 0;
+                }
+                pointsGained += 2;
+            }
+            //Check Bottom Left
+            if(row + 3 < 21 && col - 3 > 0) {
+                check1 = board[row + 1, col - 1];
+                check2 = board[row + 2, col - 2];
+                check3 = board[row + 3, col - 3];
+                if (playerColor != check1 && playerColor != check2 && playerColor == check3)
+                {
+                    board[row + 1, col - 1] = 0;
+                    board[row + 2, col - 2] = 0;
+                }
+                pointsGained += 2;
+            }
+            //Check Bottom Right
+            if(row + 3 < 21 && col + 3 < 21) {
+                check1 = board[row + 1, col + 1];
+                check2 = board[row + 2, col + 2];
+                check3 = board[row + 3, col + 3];
+                if (playerColor != check1 && playerColor != check2 && playerColor == check3)
+                {
+                    board[row + 1, col + 1] = 0;
+                    board[row + 2, col + 2] = 0;
+                }
+                pointsGained += 2;
+            }
+
+            return pointsGained;
+        }
+
+        public int CheckVictory(int[,] board, int row, int col)
+        {
+            
+            for (int i = 0; i < row; i++)
+            {
+
+            }
+        }
+        //checks each direction for the longest line
+        //return the longest line
+
+
+        public void PlayerWins(string playerName, string color)
+        {
+            Results results = new Results();// Need to add a player who won
+            results.Show();
+            this.Close();
+        }
+        //Opens up results window
+
+       
 
 
     }
