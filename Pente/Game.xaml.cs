29a3130a59828026a7e31c587c318b585b52ac79
Tobie Allen <tallen@student.neumont.edu>
2022-04-28 19:02:52 -0600

    Added playername to message boxes

--- Pente/Game.xaml.cs
+++ Pente/Game.xaml.cs
@@ -80,10 +80,10 @@ namespace Pente
                 switch (CheckVictory(board, row, col)) // Needs actual values
                 {
                     case 3:
-                        MessageBox.Show("Tria", "Lets Go!");
+                        MessageBox.Show("Tria!", currentPlayer.name);
                         break;
                     case 4:
-                        MessageBox.Show("Tessera", "Lets Go!");
+                        MessageBox.Show("Tessera!", currentPlayer.name);
                         break;
                     case 5:
                         PlayerWins(p1, p2, currentPlayer);
