e39f33e543c8adced228bdbb408164eb149adc8c
Meliemau <hwhite@student.neumont.edu>
2022-05-17 20:31:54 -0600

    Added a few methods to BlackJack

--- Casino/Blackjack.xaml.cs
+++ Casino/Blackjack.xaml.cs
@@ -20,6 +20,7 @@ namespace Casino
     public partial class Blackjack : Window
     {
         int money;
+        int bet = 0;
 
         public Blackjack(int money)
         {
@@ -43,6 +44,186 @@ namespace Casino
         {
             string message = "\t Objective: \n Win money by having the closest card total to 21, but nothing over 21. \n \t Hit: \n Take a card, up to 5 cards \n \t Stand: \n End turn \n \n Face Cards count as 10 whil Ace is 1";
             MessageBox.Show(message, "Blackjack Rules");
+        }
+
+        //Select Bet
+        private void ChipClicked(object sender, RoutedEventArgs e)
+        {
+            switch((sender as Button).Name)
+            {
+                case "btnChip1":
+
+                    if(money >= 1)
+                    {
+                        bet = 1;
+                        money -= 1;
+
+                        btnChip1.IsEnabled = false;
+                        btnChip5.IsEnabled = false;
+                        btnChip10.IsEnabled = false;
+                        btnChip20.IsEnabled = false;
+                        btnChip50.IsEnabled = false;
+                        btnChip100.IsEnabled = false;
+                        btnChip1k.IsEnabled = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Not enough funds", "ERROR");
+                    }
+
+                    break;
+                case "btnChip5":
+
+                    if(money >= 5)
+                    {
+                        bet = 5;
+                        money -= 5;
+
+                        btnChip1.IsEnabled = false;
+                        btnChip5.IsEnabled = false;
+                        btnChip10.IsEnabled = false;
+                        btnChip20.IsEnabled = false;
+                        btnChip50.IsEnabled = false;
+                        btnChip100.IsEnabled = false;
+                        btnChip1k.IsEnabled = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Not enough funds", "ERROR");
+                    }
+
+                    break;
+                case "btnChip10":
+
+                    if (money >= 10)
+                    {
+                        bet = 10;
+                        money -= 10;
+
+                        btnChip1.IsEnabled = false;
+                        btnChip5.IsEnabled = false;
+                        btnChip10.IsEnabled = false;
+                        btnChip20.IsEnabled = false;
+                        btnChip50.IsEnabled = false;
+                        btnChip100.IsEnabled = false;
+                        btnChip1k.IsEnabled = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Not enough funds", "ERROR");
+                    }
+
+                    break;
+                case "btnChip20":
+
+                    if (money >= 20)
+                    {
+                        bet = 20;
+                        money -= 20;
+
+                        btnChip1.IsEnabled = false;
+                        btnChip5.IsEnabled = false;
+                        btnChip10.IsEnabled = false;
+                        btnChip20.IsEnabled = false;
+                        btnChip50.IsEnabled = false;
+                        btnChip100.IsEnabled = false;
+                        btnChip1k.IsEnabled = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Not enough funds", "ERROR");
+                    }
+
+                    break;
+                case "btnChip50":
+
+                    if (money >= 50)
+                    {
+                        bet = 50;
+                        money -= 50;
+
+                        btnChip1.IsEnabled = false;
+                        btnChip5.IsEnabled = false;
+                        btnChip10.IsEnabled = false;
+                        btnChip20.IsEnabled = false;
+                        btnChip50.IsEnabled = false;
+                        btnChip100.IsEnabled = false;
+                        btnChip1k.IsEnabled = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Not enough funds", "ERROR");
+                    }
+
+                    break;
+                case "btnChip100":
+
+                    if (money >= 100)
+                    {
+                        bet = 100;
+                        money -= 100;
+
+                        btnChip1.IsEnabled = false;
+                        btnChip5.IsEnabled = false;
+                        btnChip10.IsEnabled = false;
+                        btnChip20.IsEnabled = false;
+                        btnChip50.IsEnabled = false;
+                        btnChip100.IsEnabled = false;
+                        btnChip1k.IsEnabled = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Not enough funds", "ERROR");
+                    }
+
+                    break;
+                case "btnChip1k":
+
+                    if (money >= 1000)
+                    {
+                        bet = 1000;
+                        money -= 1000;
+
+                        btnChip1.IsEnabled = false;
+                        btnChip5.IsEnabled = false;
+                        btnChip10.IsEnabled = false;
+                        btnChip20.IsEnabled = false;
+                        btnChip50.IsEnabled = false;
+                        btnChip100.IsEnabled = false;
+                        btnChip1k.IsEnabled = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Not enough funds", "ERROR");
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+
+            Money.Content = "$ " + money;
+        }
+
+        private void DealClicked(object sender, RoutedEventArgs e)
+        {
+            //deal cards
+
+            //stand available
+            btnStand.IsEnabled = true;
+        }
+
+        //Win
+        private void Win()
+        {
+            money += bet;
+            bet = 0;
+        }
+
+        //Loss
+        private void Loss()
+        {
+            bet = 0;
         }
     }
 }
