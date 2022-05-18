b345ea64065095374cc5f956092759e4c3c2c05a
mcone880 <79989621+mcone880@users.noreply.github.com>
2022-05-17 19:22:26 -0600

    Bets done

--- Casino/Craps.xaml.cs
+++ Casino/Craps.xaml.cs
@@ -19,17 +19,19 @@ namespace Casino
     /// </summary>
     public partial class Craps : Window
     {
-        int Money;
+        int credits;
+        int bet = 0;
         Image[] Images = new Image[12];
 
         public Craps(int money)
         {
             InitializeComponent();
-            Money = money;
+            credits = money;
             CrapsWindow.Width = 815;
             CrapsWindow.Height = 725;
             CrapsWindow.ResizeMode = ResizeMode.NoResize;
             lblMoney.Content = "Money: " + money.ToString();
+            lblBet.Content = "Bet: " + bet;
 
             int j = 0;
             for (int i = 1; i < 7; i++)
@@ -62,7 +64,7 @@ namespace Casino
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
-            GameSelection game = new GameSelection(Money);
+            GameSelection game = new GameSelection(credits);
             game.Show();
             Close();
         }
@@ -71,6 +73,11 @@ namespace Casino
         {
             var results = Roll();
             SetImages(results[0], results[1]);
+
+
+
+            bet = 0;
+            lblBet.Content = "Bet: " + bet;
         }
 
         private void SetImages(int dice1, int dice2)
@@ -84,6 +91,18 @@ namespace Casino
             Image image2 = (Image)FindName("Col2Dice" + dice2);
             image1.Visibility = Visibility.Visible;
             image2.Visibility = Visibility.Visible;
+        }
+
+        private void btnClick(object sender, RoutedEventArgs e)
+        {
+            if (sender == btn1 && bet + 1 <= credits) bet += 1;
+            else if (sender == btn5 && bet + 5 <= credits) bet += 5;
+            else if (sender == btn10 && bet + 10 <= credits) bet += 10;
+            else if (sender == btn20 && bet + 20 <= credits) bet += 20;
+            else if (sender == btn50 && bet + 50 <= credits) bet += 50;
+            else if (sender == btn100 && bet + 100 <= credits) bet += 100;
+            else if (sender == btn1000 && bet + 1000 <= credits) bet += 1000; 
+            lblBet.Content = "Bet: " + bet;
         }
     }
 }
