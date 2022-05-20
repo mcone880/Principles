aa482a8ef9c3a889c224f799ef073ec9ffcea728
Tobie Allen <tallen@student.neumont.edu>
2022-05-19 18:29:30 -0600

    Poker - slight UI changes and working on Draw click

--- Casino/Poker.xaml.cs
+++ Casino/Poker.xaml.cs
@@ -20,11 +20,18 @@ namespace Casino
     public partial class Poker : Window
     {
         int money;
+        bool firstDraw = false; 
 
         public Poker(int money)
         {
             InitializeComponent();
             this.money = money;
+            txtMoney.Text = "$" + money; 
+        }
+
+        private void CheckWin() // do this
+        {
+            //
         }
 
         private void btnBack_Click(object sender, RoutedEventArgs e) // goes back to the game menu and closes this window 
@@ -47,9 +54,29 @@ namespace Casino
 
         private void btnDraw_Click(object sender, RoutedEventArgs e) // places bet and draws cards 
         {
+            // bet stuff 
+
+            // if firstDraw is false, then draw cards and disable back button and set firstDraw to true 
+            if (firstDraw == false)
+            {
+                btnBack.IsEnabled = false;
+
+                // draw cards 
+
+                firstDraw = true; 
+            }
+
+            // if firstDraw is true, redraw cards for ones not held and check wins and set firstDraw to false
+            else
+            {
+                btnBack.IsEnabled = true;
+
+                // draw cards 
+
+                firstDraw = false;
+            }
 
         }
-
 
         // Chips Buttons
         private void btnChip1_Click(object sender, RoutedEventArgs e)
