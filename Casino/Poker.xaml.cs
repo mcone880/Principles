6123d5b022173c3375281ab62f7f0447446fd9e7
Tobie Allen <tallen@student.neumont.edu>
2022-05-19 19:54:45 -0600

    Poker - reset button adding and working, draw cards working

--- Casino/Poker.xaml.cs
+++ Casino/Poker.xaml.cs
@@ -21,13 +21,71 @@ namespace Casino
     {
         int money;
         int bank; //Do not alter or Change, This is so that we can keep track of our Bank Amount ~ Tommy
-        bool firstDraw = false; 
+        bool firstDraw = false;
+        bool gameSet = true;
+        Deck deck = new Deck();
+
+        Cards card1, card2, card3, card4, card5;
 
         public Poker(int money, int bank)
         {
             InitializeComponent();
             this.money = money;
             this.bank = bank;
+
+            txtMoney.Text = "$" + money;
+
+            deck.Reset();
+            deck.Shuffle();
+
+            ResetCardButtonImage(btnHold1);
+            ResetCardButtonImage(btnHold2);
+            ResetCardButtonImage(btnHold3);
+            ResetCardButtonImage(btnHold4);
+            ResetCardButtonImage(btnHold5);
+        }
+
+        private void CheckWin() // need to do this 
+        {
+            MessageBox.Show("Made it to the win check! Unfortunately, I have not yet made the win check.");
+        }
+
+        private void SetCardButtonImage(Button button, Cards card)
+        {
+            string cardImageURL = deck.DisplayCard(card);
+
+            BitmapImage bitimg = new BitmapImage();
+            bitimg.BeginInit();
+            bitimg.UriSource = new Uri(cardImageURL, UriKind.RelativeOrAbsolute);
+            bitimg.EndInit();
+
+            Image img = new Image();
+            img.Stretch = Stretch.Fill;
+            img.Source = bitimg;
+
+            // Set Button.Content
+            button.Content = img;
+
+            // Set Button.Background
+            button.Background = new ImageBrush(bitimg);
+        }
+
+        private void ResetCardButtonImage(Button button)
+        {
+            BitmapImage bitimg = new BitmapImage();
+            bitimg.BeginInit();
+            bitimg.UriSource = new Uri("/Assets/Blackjack/Cards/cardBack_blue1.png", UriKind.RelativeOrAbsolute);
+            bitimg.EndInit();
+
+            Image img = new Image();
+            img.Stretch = Stretch.Fill;
+            img.Source = bitimg;
+
+            // Set Button.Content
+            button.Content = img;
+
+            // Set Button.Background
+            button.Background = new ImageBrush(bitimg);
         }
 
         private void btnBack_Click(object sender, RoutedEventArgs e) // goes back to the game menu and closes this window 
@@ -44,34 +102,125 @@ namespace Casino
                 "- Choose Which Cards to Keep\n" +
                 "- Click Draw Button Again to Get New Cards\n" +
                 "- Claim Winnings Based On Cards Drawn\n\n" +
-                "- Play Again or Exit\n", 
+                "- Click Reset to Play Again or Exit\n", 
                 "Poker Rules");
         }
 
         private void btnDraw_Click(object sender, RoutedEventArgs e) // places bet and draws cards 
         {
-            // bet stuff 
-
-            // if firstDraw is false, then draw cards and disable back button and set firstDraw to true 
-            if (firstDraw == false)
+            if (gameSet == true)
             {
-                btnBack.IsEnabled = false;
+                // bet stuff 
 
-                // draw cards 
+                // if firstDraw is false, then draw cards and disable back button and set firstDraw to true 
+                if (firstDraw == false)
+                {
+                    // enable hold buttons
+                    btnHold1.IsEnabled = true;
+                    btnHold2.IsEnabled = true;
+                    btnHold3.IsEnabled = true;
+                    btnHold4.IsEnabled = true;
+                    btnHold5.IsEnabled = true;
 
-                firstDraw = true; 
+                    deck.Shuffle();
+                    btnBack.IsEnabled = false;
+
+                    // draw cards 
+                    card1 = deck.TakeCard();
+                    card2 = deck.TakeCard();
+                    card3 = deck.TakeCard();
+                    card4 = deck.TakeCard();
+                    card5 = deck.TakeCard();
+
+                    SetCardButtonImage(btnHold1, card1);
+                    SetCardButtonImage(btnHold2, card2);
+                    SetCardButtonImage(btnHold3, card3);
+                    SetCardButtonImage(btnHold4, card4);
+                    SetCardButtonImage(btnHold5, card5);
+
+                    firstDraw = true;
+                }
+
+                // if firstDraw is true, redraw cards for ones not held and check wins and set firstDraw to false
+                else
+                {
+                    // disable hold cards
+                    btnHold1.IsEnabled = false;
+                    btnHold2.IsEnabled = false;
+                    btnHold3.IsEnabled = false;
+                    btnHold4.IsEnabled = false;
+                    btnHold5.IsEnabled = false;
+
+                    btnBack.IsEnabled = true;
+
+                    // hold cards 
+                    if (card1.isHeld == false)
+                    {
+                        card1 = deck.TakeCard();
+                        SetCardButtonImage(btnHold1, card1);
+                    }
+                    if (card2.isHeld == false)
+                    {
+                        card2 = deck.TakeCard();
+                        SetCardButtonImage(btnHold2, card2);
+                    }
+                    if (card3.isHeld == false)
+                    {
+                        card3 = deck.TakeCard();
+                        SetCardButtonImage(btnHold3, card3);
+                    }
+                    if (card4.isHeld == false)
+                    {
+                        card4 = deck.TakeCard();
+                        SetCardButtonImage(btnHold4, card4);
+                    }
+                    if (card5.isHeld == false)
+                    {
+                        card5 = deck.TakeCard();
+                        SetCardButtonImage(btnHold5, card5);
+                    }
+
+                    CheckWin();
+                    firstDraw = false;
+                    gameSet = false;
+                }
+            }
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e) // reset game 
+        {
+            // disable hold cards
+            btnHold1.IsEnabled = false;
+            btnHold2.IsEnabled = false;
+            btnHold3.IsEnabled = false;
+            btnHold4.IsEnabled = false;
+            btnHold5.IsEnabled = false;
+
+            // cards not held
+            if (card1 != null)
+            {
+                card1.isHeld = false;
+                card2.isHeld = false;
+                card3.isHeld = false;
+                card4.isHeld = false;
+                card5.isHeld = false;
             }
 
-            // if firstDraw is true, redraw cards for ones not held and check wins and set firstDraw to false
-            else
-            {
-                btnBack.IsEnabled = true;
+            // hold word not visible 
+            lblHold1.Visibility = Visibility.Hidden;
+            lblHold2.Visibility = Visibility.Hidden;
+            lblHold3.Visibility = Visibility.Hidden;
+            lblHold4.Visibility = Visibility.Hidden;
+            lblHold5.Visibility = Visibility.Hidden;
 
-                // draw cards 
+            // cards picture set back to card back
+            ResetCardButtonImage(btnHold1);
+            ResetCardButtonImage(btnHold2);
+            ResetCardButtonImage(btnHold3);
+            ResetCardButtonImage(btnHold4);
+            ResetCardButtonImage(btnHold5);
 
-                firstDraw = false;
-            }
-
+            gameSet = true;
         }
 
         // Chips Buttons
@@ -113,34 +262,72 @@ namespace Casino
         // Hold Cards
         private void btnHold1_Click(object sender, RoutedEventArgs e)
         {
-            if (lblHold1.Visibility == Visibility.Hidden) lblHold1.Visibility = Visibility.Visible;
-            else lblHold1.Visibility = Visibility.Hidden;
-
+            if (lblHold1.Visibility == Visibility.Hidden) // if hidden (not held) then make visible and hold card
+            {
+                lblHold1.Visibility = Visibility.Visible;
+                card1.isHeld = true;
+            }
+            else
+            {
+                card1.isHeld = false;
+                lblHold1.Visibility = Visibility.Hidden;
+            }
         }
 
         private void btnHold2_Click(object sender, RoutedEventArgs e)
         {
-            if (lblHold2.Visibility == Visibility.Hidden) lblHold2.Visibility = Visibility.Visible;
-            else lblHold2.Visibility = Visibility.Hidden;
-
+            if (lblHold2.Visibility == Visibility.Hidden)
+            {
+                card2.isHeld = true;
+                lblHold2.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                card2.isHeld = false; 
+                lblHold2.Visibility = Visibility.Hidden;
+            }
         }
 
         private void btnHold3_Click(object sender, RoutedEventArgs e)
         {
-            if (lblHold3.Visibility == Visibility.Hidden) lblHold3.Visibility = Visibility.Visible;
-            else lblHold3.Visibility = Visibility.Hidden;
+            if (lblHold3.Visibility == Visibility.Hidden)
+            {
+                card3.isHeld = true; 
+                lblHold3.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                card3.isHeld = false; 
+                lblHold3.Visibility = Visibility.Hidden;
+            }
         }
 
         private void btnHold4_Click(object sender, RoutedEventArgs e)
         {
-            if (lblHold4.Visibility == Visibility.Hidden) lblHold4.Visibility = Visibility.Visible;
-            else lblHold4.Visibility = Visibility.Hidden;
+            if (lblHold4.Visibility == Visibility.Hidden)
+            {
+                card4.isHeld = true; 
+                lblHold4.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                card4.isHeld = false;
+                lblHold4.Visibility = Visibility.Hidden;
+            }
         }
 
         private void btnHold5_Click(object sender, RoutedEventArgs e)
         {
-            if (lblHold5.Visibility == Visibility.Hidden) lblHold5.Visibility = Visibility.Visible;
-            else lblHold5.Visibility = Visibility.Hidden;
+            if (lblHold5.Visibility == Visibility.Hidden)
+            {
+                card5.isHeld = true; 
+                lblHold5.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                card5.isHeld = false; 
+                lblHold5.Visibility = Visibility.Hidden;
+            }
         }
     }
 }
