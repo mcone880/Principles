9d309dcd31d39eea28baf6c240221376581717a9
Ghostninja2451 <80051664+Ghostninja2451@users.noreply.github.com>
2022-05-24 19:36:18 -0600

    Roulett Finished(I think)

--- Casino/Roulette.xaml.cs
+++ Casino/Roulette.xaml.cs
@@ -25,6 +25,7 @@ namespace Casino
         public int numbers = 0;
         public string color = "";
         public int bettingPool = 0;
+        public int totalPool = 0;
         
         private int one = 0;
         private string onestr = "1";
@@ -281,7 +282,7 @@ namespace Casino
                         twentySeven += bettingPool;
                         money -= bettingPool;
                     }
-                    if (twentyEight.Equals(btn.Content.ToString()))
+                    if (twetyEightStr.Equals(btn.Content.ToString()))
                     {
                         twentyEight += bettingPool;
                         money -= bettingPool;
@@ -586,61 +587,306 @@ namespace Casino
             if (color.Equals(redStr) && red > 0)
             {
                 money += red + red;
+                totalPool += red + red;
+                red = 0;
             }
             if (color.Equals(blackStr) && black > 0)
             {
                 money += black + black;
+                totalPool += black + black;
+                black = 0;
             }
             //Odd / Even - 1:1
             if (numbers == one || numbers == three || numbers == five || numbers == seven || numbers == nine || numbers == eleven || numbers == thirteen || numbers == fifteen || numbers == seventeen || numbers == nineteen || numbers == twentyOne || numbers == twentyThree || numbers == twentyFive || numbers == twentySeven || numbers == twentyNine || numbers == thirtyOne || numbers == thirtyThree || numbers == thirtyFive && odd > 0)
             {
                 money += odd + odd;
+                totalPool += odd + odd;
                 odd = 0;
             }
             if (numbers == two || numbers == four || numbers == six || numbers == eight || numbers == ten || numbers == twelve || numbers == fourteen || numbers == sixteen || numbers == eighteen || numbers == twenty || numbers == twentyTwo || numbers == twentyFour || numbers == twentySix|| numbers == twentyEight || numbers == thirty || numbers == thirtyTwo || numbers == thirtyFour || numbers == thirtySix && even > 0)
             {
                 money += even + even;
+                totalPool += even + even;
                 even = 0;
             }
             //High 19 - 36 / Low 1 - 18 - 1:1
             if (numbers == one || numbers == two || numbers == three || numbers == four || numbers == five || numbers == six || numbers == seven || numbers == eight || numbers == nine || numbers == ten || numbers == eleven || numbers == twelve || numbers == thirty || numbers == fourteen || numbers == fifteen || numbers == sixteen || numbers == seventeen || numbers == eighteen && firstHalf > 0)
             {
                 money += firstHalf + firstHalf;
+                totalPool += firstHalf + firstHalf;
                 firstHalf = 0;
             }
             if (numbers == nineteen || numbers == twenty || numbers == twentyOne || numbers == twentyTwo || numbers == twentyThree || numbers == twentyFour || numbers == twentyFive || numbers == twentySix || numbers == twentySeven || numbers == twentyEight || numbers == twentyNine || numbers == thirty || numbers == thirtyOne || numbers == thirtyTwo || numbers == thirtyThree || numbers == thirtyFour || numbers == thirtyFive || numbers == thirtySix && secondHalf > 0)
             {
                 money += secondHalf + secondHalf;
+                totalPool += secondHalf + secondHalf;
                 secondHalf = 0;
             }
             //Dozen - 3:1
             //first dozen
-            if (numbers == 1 || numbers == 2 || numbers == 3 || numbers == 4 || numbers == 5 || numbers == 6 || numbers == 7 || numbers == 8 || numbers == 9 || numbers == 10 || numbers == 11 || numbers == 12 && first12 > 0)
+            if (numbers == one || numbers == two || numbers == three || numbers == four || numbers == five || numbers == six || numbers == seven || numbers == eight || numbers == nine || numbers == ten || numbers == eleven || numbers == twelve && first12 > 0)
             {
                 money += first12 + (3 * first12);
+                totalPool += first12 + (3 * first12);
                 first12 = 0;
             }
             //second dozen
-            if (numbers == 13 || numbers == 14 || numbers == 15 || numbers == 16 || numbers == 17 || numbers == 18 || numbers == 19 || numbers == 20 || numbers == 21 || numbers == 22 || numbers == 23 || numbers == 24 && second12 > 0)
+            if (numbers == thirteen || numbers == fourteen || numbers == fifteen || numbers == sixteen || numbers == seventeen || numbers == eighteen || numbers == nineteen || numbers == twenty || numbers == twentyOne || numbers == twentyTwo || numbers == twentyThree || numbers == twentyFour && second12 > 0)
             {
                 money += second12 + (3 * second12);
+                totalPool += second12 + (3 * second12);
                 first12 = 0;
             }
             //third dozen
-            if (numbers == 25 || numbers == 26 || numbers == 27 || numbers == 28 || numbers == 29 || numbers == 30 || numbers == 31 || numbers == 32 || numbers == 33 || numbers == 34 || numbers == 35 || numbers == 36 && third12 > 0)
+            if (numbers == twentyFive || numbers == twentySix || numbers == twentySeven || numbers == twentyEight || numbers == twentyNine || numbers == thirty || numbers == thirtyOne || numbers == thirtyTwo || numbers == thirtyThree || numbers == thirtyFour || numbers == thirtyFive || numbers == thirtySix && third12 > 0)
             {
                 money += third12 + (3 * third12);
+                totalPool += third12 + (3 * third12);
                 first12 = 0;
             }
             //Column - 3:1 
             //col1
-            if (numbers == one || numbers == four || numbers == seven)
+            if (numbers == one || numbers == four || numbers == seven || numbers == ten || numbers == thirteen || numbers == sixteen || numbers == nineteen || numbers == twentyTwo || numbers == twentyFive || numbers == twentyEight || numbers == thirtyOne || numbers == thirtyFour && column1 > 0 )
             {
-
+                money += column1 + (3 * column1);
+                totalPool += column1 + (3 * column1);
+                column1 = 0;
+            }
+            //col2
+            if (numbers == two || numbers == five || numbers == eight || numbers == eleven || numbers == fourteen || numbers == seventeen || numbers == twenty || numbers == twentyThree || numbers == twentyNine || numbers == thirtyTwo || numbers == thirtyFive && column2 > 0)
+            {
+                money += column2 + (3 * column2);
+                totalPool += column2 + (3 * column2);
+                column2 = 0;
+            }
+            //col3
+            if (numbers == three || numbers == six || numbers == nine || numbers == twelve || numbers == fifteen || numbers == eighteen || numbers == twentyOne || numbers == twentyFour || numbers == twentySeven || numbers == thirty || numbers == thirtyThree || numbers == thirtySix && column3 > 0)
+            {
+                money += column3 + (3 * column3);
+                totalPool += column3 + (3 * column3);
+                column3 = 0;
             }
             //Straight Number(1 - Number) -36:1
+            if (numbers == one)
+            {
+                money += one + (36 * one);
+                totalPool += one + (36 * one);
+                one = 0;
+            }
+            if (numbers == two)
+            {
+                money += two + (36 * two);
+                totalPool += two + (36 * two);
+                two = 0;
+            }
+            if (numbers == three)
+            {
+                money += three + (36 * three);
+                totalPool += three + (36 * three);
+                three = 0;
+            }
+            if (numbers == four)
+            {
+                money += four + (36 * four);
+                totalPool += four + (36 * four);
+                four = 0;
+            }
+            if (numbers == five)
+            {
+                money += five + (36 * five);
+                totalPool += five + (36 * five);
+                five = 0;
+            }
+            if (numbers == six)
+            {
+                money += six + (36 * six);
+                totalPool += six + (36 * six);
+                six = 0;
+            }
+            if (numbers == seven)
+            {
+                money += seven + (36 * seven);
+                totalPool += seven + (36 * seven);
+                seven = 0;
+            }
+            if (numbers == eight)
+            {
+                money += eight + (36 * eight);
+                totalPool += eight + (36 * eight);
+                eight = 0;
+            }
+            if (numbers == nine)
+            {
+                money += nine + (36 * nine);
+                totalPool += nine + (36 * nine);
+                nine = 0;
+            }
+            if (numbers == ten)
+            {
+                money += ten + (36 * ten);
+                totalPool += ten + (36 * ten);
+                ten = 0;
+            }
+            if (numbers == eleven)
+            {
+                money += eleven + (36 * eleven);
+                totalPool += eleven + (36 * eleven);
+                eleven = 0;
+            }
+            if (numbers == twelve)
+            {
+                money += twelve + (36 * twelve);
+                totalPool += twelve + (36 * twelve);
+                twelve = 0;
+            }
+            if (numbers == thirteen)
+            {
+                money += thirteen + (36 * thirteen);
+                totalPool += thirteen + (36 * thirteen);
+                thirteen = 0;
+            }
+            if (numbers == fourteen)
+            {
+                money += fourteen + (36 * fourteen);
+                totalPool += fourteen + (36 * fourteen);
+                fourteen = 0;
+            }
+            if (numbers == fifteen)
+            {
+                money += fifteen + (36 * fifteen);
+                totalPool += fifteen + (36 * fifteen);
+                fifteen = 0;
+            }
+            if (numbers == sixteen)
+            {
+                money += sixteen + (36 * sixteen);
+                totalPool += sixteen + (36 * sixteen);
+                sixteen = 0;
+            }
+            if (numbers == seventeen)
+            {
+                money += seventeen + (36 * seventeen);
+                totalPool += seventeen + (36 * seventeen);
+                seventeen = 0;
+            }
+            if (numbers == eighteen)
+            {
+                money += eighteen + (36 * eighteen);
+                totalPool += eighteen + (36 * eighteen);
+                eighteen = 0;
+            }
+            if (numbers == nineteen)
+            {
+                money += nineteen + (36 * nineteen);
+                totalPool += nineteen + (36 * nineteen);
+                nineteen = 0;
+            }
+            if (numbers == twenty)
+            {
+                money += twenty + (36 * twenty);
+                totalPool += twenty + (36 * twenty);
+                twenty = 0;
+            }
+            if (numbers == twentyOne)
+            {
+                money += twentyOne + (36 * twentyOne);
+                totalPool += twentyOne + (36 * twentyOne);
+                twentyOne = 0;
+            }
+            if (numbers == twentyTwo)
+            {
+                money += twentyTwo + (36 * twentyTwo);
+                totalPool += twentyTwo + (36 * twentyTwo);
+                twentyTwo = 0;
+            }
+            if (numbers == twentyThree)
+            {
+                money += twentyThree + (36 * twentyThree);
+                totalPool += twentyThree + (36 * twentyThree);
+                twentyThree = 0;
+            }
+            if (numbers == twentyFour)
+            {
+                money += twentyFour + (36 * twentyFour);
+                totalPool += twentyFour + (36 * twentyFour);
+                twentyFour = 0;
+            }
+            if (numbers == twentyFive)
+            {
+                money += twentyFive + (36 * twentyFive);
+                totalPool += twentyFive + (36 * twentyFive);
+                twentyFive = 0;
+            }
+            if (numbers == twentySix)
+            {
+                money += twentySix + (36 * twentySix);
+                totalPool += twentySix + (36 * twentySix);
+                twentySix = 0;
+            }
+            if (numbers == twentySeven)
+            {
+                money += twentySeven + (36 * twentySeven);
+                totalPool += twentySeven + (36 * twentySeven);
+                twentySeven = 0;
+            }
+            if (numbers == twentyEight)
+            {
+                money += twentyEight + (36 * twentyEight);
+                totalPool += twentyEight + (36 * twentyEight);
+                twentyEight = 0;
+            }
+            if (numbers == twentyNine)
+            {
+                money += twentyNine + (36 * twentyNine);
+                totalPool += twentyNine + (36 * twentyNine);
+                twentyNine = 0;
+            }
+            if (numbers == thirty)
+            {
+                money += thirty + (36 * thirty);
+                totalPool += thirty + (36 * thirty);
+                thirty = 0;
+            }
+            if (numbers == thirtyOne)
+            {
+                money += thirtyOne + (36 * thirtyOne);
+                totalPool += thirtyOne + (36 * thirtyOne);
+                thirtyOne = 0;
+            }
+            if (numbers == thirtyTwo)
+            {
+                money += thirtyTwo + (36 * thirtyTwo);
+                totalPool += thirtyTwo + (36 * thirtyTwo);
+                thirtyTwo = 0;
+            }
+            if (numbers == thirtyThree)
+            {
+                money += thirtyThree + (36 * thirtyThree);
+                totalPool += thirtyThree + (36 * thirtyThree);
+                thirtyThree = 0;
+            }
+            if (numbers == thirtyFour)
+            {
+                money += thirtyFour + (36 * thirtyFour);
+                totalPool += thirtyFour + (36 * thirtyFour);
+                thirtyFour = 0;
+            }
+            if (numbers == thirtyFive)
+            {
+                money += thirtyFive + (36 * thirtyFive);
+                totalPool += thirtyFive + (36 * thirtyFive);
+                thirtyFive = 0;
+            }
+            if (numbers == thirtySix)
+            {
+                money += thirtySix + (36 * thirtySix);
+                totalPool += thirtySix + (36 * thirtySix);
+                thirtySix = 0;
+            }
 
             Currency.Content = "Amount: $" + money.ToString();
+            BettingAmount.Content = "Winning: $" + totalPool.ToString();
+            totalPool = 0;
         }
 
         //Betting
